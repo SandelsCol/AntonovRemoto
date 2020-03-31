7c5da994af54a236d93114f15db3e111b90e1e4e
SandelsCol <62896050+SandelsCol@users.noreply.github.com>
2020-03-30 20:11:04 -0500

    Segunda parte

--- Answer.py
+++ Answer.py
@@ -1,15 +1,28 @@
 print("Bienvenido a la aerolinea Delvalle Airlines")
+
 nacional=1
 intercontinental=2
 
 vuelo=int(input("Digite 1 si su vuelo es nacional , digite 2 si su vuelo es intercontinental"))
 kg=int(input("Digite el peso total en kilos del paquete"))
+km=int(input("Digite la cantidad de kilometros del paquete"))
 
-if vuelo==1:
-    km=int(input("Digite la cantidad de kilometros del paquete"))
-    pb=4500*kg + 4000*km
-    print("El precio base es..",pb)
+if kg >= 10:
+    if vuelo==1:
+        if kg>100:
+            desc=(4500*kg + 4000*km)*0.15
+            pb=4500*kg + 4000*km - desc
+            print("El precio base de su envio nacional con su descuento es",pb,"pesos")
+        else:
+            pb=4500*kg + 4000*km
+            print("El precio base de su envio nacional es..",pb,"pesos")
+    else:
+        if km>8000 and kg>400:
+            desc=(4500*kg + 8000*(0.621371*km))*0.10
+            pb=4500*kg + 8000*(0.621371*km) - desc
+            print("El precio base de su envio intercontiental con descuento es",pb,"pesos")
+        else:
+            pb=4500*kg + 8000*(0.621371*km)
+            print("El precio base de su envio intercontinental es..",pb,"pesos")    
 else:
-    milla=int(input("Digite la distancia total en millas del paquete"))
-    pb=4500*kg + 8000*milla
-    print("El precio base es..",pb)    
+    print("No aceptamos paquetes de menos de 10kg")         
